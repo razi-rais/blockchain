1c6e3ac4d363581a7eb38f24ab0db640960600f0
Razi Rais <razibinrais@outlook.com>
2017-10-06 11:06:45 -0400

    Adding support to store hash of actual contracts

--- nyc-blockchain-event/Sale.sol
+++ nyc-blockchain-event/Sale.sol
@@ -1,35 +1,46 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.17;
 
-contract Sale
+contract Sales
 {
     mapping(string => uint) items;
+    mapping (string => string) itemHash;
     uint public totalItems = 0;
-    uint public itemsCount =0;
+    uint public currentItemsCount =0;
 
-    function Sale(uint totalItemCount){
+
+    function Sales(uint totalItemCount) public{
         totalItems = totalItemCount;
+        currentItemsCount = 0;
     }
     
-    function AddItem(string name, uint allowedQuantity){
-        if (itemsCount < totalItems ){
+    function AddItem(string name, uint allowedQuantity,string hash) public {
+        if (currentItemsCount < totalItems ){
             if(items[name] == 0 ){
                 items[name] = allowedQuantity;
-                itemsCount++;
+                itemHash[name] = hash;
+                currentItemsCount++;
             }
         }
     }
-    
-        
-    function DecreaseItemCount(string name, uint qty){
+
+    function DecreaseItemCount(string name, uint qty) public {
          uint currentQty = items[name];
          if(currentQty >= qty){
               items[name] =  currentQty - qty;
          } 
+         
     }
     
-    function GetItemQty(string name) constant returns (uint){
+    function GetItemQty(string name) constant public returns (uint) {
         
             return items[name];
             
         }
+    
+    function GetItemHash(string name) constant public returns (string){
+        
+            return itemHash[name];
+        
+    }
+    
 }
