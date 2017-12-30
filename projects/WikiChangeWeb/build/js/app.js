bfe24438f9b5a0c9c28ddc396f782ff662007faa
Crystal Tenn <crtenn@microsoft.com>
2017-12-30 08:33:29 +0000

    add wikipedia API functionality to get ID for an article by title

--- projects/WikiChangeWeb/build/js/app.js
+++ projects/WikiChangeWeb/build/js/app.js
@@ -14,37 +14,33 @@
 });
 
 $('#save-button').click(function () {
+    var userId = $('#userId').val().toString();
+    var title = $('#wiki-title-1').val().toString();
 
-    // call wikipedia to get ID for the article
-    // https://stackoverflow.com/questions/6168020/what-is-wikipedia-pageid-how-to-change-it-into-real-page-url
-    // 
+    var urlTitle = title.replace(/ /g, "_");
+    var url = "https://en.wikipedia.org/wiki/" + urlTitle;
 
-    // query by title of article, get ID out
+    var linkAPI = "https://en.wikipedia.org/w/api.php?action=query&prop=pageprops&ppprop=wikibase_item&format=json&titles=" + title + "&callback=?"
+    $.getJSON(linkAPI, function (json) {
+        id = Object.keys(json.query.pages)[0];
 
-    // (remove ID from HTML page)
-
-    $.ajax({
-        url: apiUrl + '/SaveArticle',
-        data: JSON.stringify({
-            "UserID": "12",
-            "Articles":
-
-            [{
-                "ID": "1",
-                "Url": "ASD",
-                "Title": "ASD is Great"
-            },
-            {
-                "ID": "5",
-                "Url": "Mustard",
-                "Title": "Mustard is bad"
-            }]
-        }),
-        contentType: 'application/json',
-        type: 'POST'
-    }).done(function (data) {
-        $('#div-response').text(data.message);
-    }).fail(function (error) {
-        $('#info').html('<p>An error has occurred</p>');
+        $.ajax({
+            url: apiUrl + '/SaveArticle',
+            data: JSON.stringify({
+                "UserID": userId,
+                "Articles":
+                [{
+                    "ID": id,
+                    "Url": url,
+                    "Title": title
+                }]
+            }),
+            contentType: 'application/json',
+            type: 'POST'
+        }).done(function (data) {
+            $('#div-response').text(data.message);
+        }).fail(function (error) {
+            $('#info').html('<p>An error has occurred</p>');
+        });
     });
 });
