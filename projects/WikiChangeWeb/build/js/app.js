01b7d4aeac65e1aeeb4dbf0c8f11540ce806dde5
Crystal Tenn <crtenn@microsoft.com>
2018-01-02 06:00:22 +0000

    web3js working

--- projects/WikiChangeWeb/build/js/app.js
+++ projects/WikiChangeWeb/build/js/app.js
@@ -1,19 +1,25 @@
 ﻿$(document).ready(function () {
-    //var web3 = require("web3");
+    var contractId = "0x551db3f1ee78e4e35b337fd10589719bb2f14df4"
+    var abi = [{ "constant": false, "inputs": [{ "name": "id", "type": "uint256" }, { "name": "title", "type": "string" }, { "name": "url", "type": "string" }, { "name": "revision_old", "type": "uint256" }, { "name": "revision_new", "type": "uint256" }, { "name": "timestamp", "type": "uint256" }, { "name": "change_type", "type": "string" }, { "name": "user", "type": "string" }, { "name": "comment", "type": "string" }], "name": "UpdateArticleHistory", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "", "type": "address" }, { "indexed": false, "name": "id", "type": "uint256" }, { "indexed": false, "name": "title", "type": "string" }, { "indexed": false, "name": "url", "type": "string" }, { "indexed": false, "name": "revision_old", "type": "uint256" }, { "indexed": false, "name": "revision_new", "type": "uint256" }, { "indexed": false, "name": "timestamp", "type": "uint256" }, { "indexed": false, "name": "change_type", "type": "string" }, { "indexed": false, "name": "user", "type": "string" }, { "indexed": false, "name": "comment", "type": "string" }], "name": "articleUpdateEvent", "type": "event" }]
+    var httpProvider = "http://localhost:8545";
 
-    //contractId = "0x551db3f1ee78e4e35b337fd10589719bb2f14df4"
-    //abi = [{ "constant": false, "inputs": [{ "name": "id", "type": "uint256" }, { "name": "title", "type": "string" }, { "name": "url", "type": "string" }, { "name": "revision_old", "type": "uint256" }, { "name": "revision_new", "type": "uint256" }, { "name": "timestamp", "type": "uint256" }, { "name": "change_type", "type": "string" }, { "name": "user", "type": "string" }, { "name": "comment", "type": "string" }], "name": "UpdateArticleHistory", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "", "type": "address" }, { "indexed": false, "name": "id", "type": "uint256" }, { "indexed": false, "name": "title", "type": "string" }, { "indexed": false, "name": "url", "type": "string" }, { "indexed": false, "name": "revision_old", "type": "uint256" }, { "indexed": false, "name": "revision_new", "type": "uint256" }, { "indexed": false, "name": "timestamp", "type": "uint256" }, { "indexed": false, "name": "change_type", "type": "string" }, { "indexed": false, "name": "user", "type": "string" }, { "indexed": false, "name": "comment", "type": "string" }], "name": "articleUpdateEvent", "type": "event" }]
-    //httpProvider = "http://localhost:8545";
+    var web3 = new Web3(new Web3.providers.HttpProvider(httpProvider));
+    var contractInstance = web3.eth.contract(abi).at(contractId);
 
-    //web3 = new Web3(new Web3.providers.HttpProvider(httpProvider));
-    //contractInstance = web3.eth.contract(newAbi).at(contractId);
+    web3.eth.getAccounts(console.log);
 
-    ////Web3 = require('web3');
-    ////web3 = new Web3(provider);
-    ////var contract = new web3.eth.Contract(abi, contract_address)
+    contractInstance.allEvents(console.log);
 
-    //web3.eth.getAccounts().then((accounts) => { console.log('accounts', accounts); });
-    //contractInstance.getPastEvents('articleUpdateEvent', { fromBlock: 0, toBlock: 'latest' }).then(function (events) { console.log(events) }); 
+    //contractInstance.getPastEvents('articleUpdatedEvent', {
+    //    fromBlock: 0,
+    //    toBlock: 'latest'
+    //}, function (error, events) { console.log(events); })
+    //    .then(function (events) {
+    //        console.log(events) // same results as the optional callback above
+    //    });
+
+
+    //contractInstance.getPastEvents('articleUpdateEvent', { fromBlock: 0, toBlock: 'latest' }).then(function (events) { console.log(events) });
 });
 
 
