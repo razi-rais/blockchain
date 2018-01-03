6b0e66db650e881edc6c3dfbc1bd81fb096bb300
Viktor Dikov <viktordd@gmail.com>
2018-01-02 22:38:38 -0500

    Added web3

--- projects/WikiChangeAngular/src/app/app.component.ts
+++ projects/WikiChangeAngular/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Web3Service } from './web3/web3.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   bodyClass = 'nav-md';
+
+  constructor(private web3Service: Web3Service) {
+
+  }
+
+  ngOnInit() {
+    this.web3Service.init();
+  }
+
 }
